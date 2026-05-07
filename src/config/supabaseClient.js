a480f4829675d569bf67be0b1e3f9fc418dbceba
Dan Mountain <danmountain@Dans-MacBook-Pro.local>
2026-05-06 23:40:49 -0400

    fix supabase connection

--- src/config/supabaseClient.js
+++ src/config/supabaseClient.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
-const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const supabaseUrl = 'https://wsynwtpmbagonchbpqvn.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndzeW53dHBtYmFnb25jaGJwcXZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzYxODM1ODQsImV4cCI6MjA5MTc1OTU4NH0.4jFRUa3kyh98bwcL7vFyj3wAdJXUjXdkxSS6fwZzzy8'
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
